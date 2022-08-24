8937a882aae8fb27151ee68c02b40cfa3a0e7022
Student <meetmpatel20-@gnu.ac.in>
2022-08-24 10:46:14 +0530

    initial commit

---
+++ src/main/kotlin/pr1_5.kt
@@ -0,0 +1,22 @@
+import java.util.Scanner
+
+fun main() {
+    print("Enter Month Number: ")
+    val read = Scanner(System.`in`)
+    val n:Int = read.nextInt()
+    when(n) {
+        1 -> print("January")
+        2 -> print("February")
+        3 -> print("March")
+        4 -> print("April")
+        5 -> print("May")
+        6 -> print("June")
+        7 -> print("July")
+        8 -> print("August")
+        9 -> print("September")
+        10 -> print("October")
+        11 -> print("November")
+        12 -> print("December")
+        else -> print("please enter proper number")
+    }
+}
