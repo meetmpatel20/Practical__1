7fc0599f0e730132299e3fd53a1da420af195da7
Student <meetmpatel20-@gnu.ac.in>
2022-08-24 10:44:24 +0530

    initial commit

---
+++ src/main/kotlin/pr1_4.kt
@@ -0,0 +1,16 @@
+import java.util.Scanner
+
+fun main(){
+    val read = Scanner(System.`in`)
+    println("Enter number")
+    val n = read.nextInt()
+    val ans = if (n % 2 == 0){
+        "Number is even"
+    } else {
+        "Number is odd"
+    }
+    println(ans)
+}
+
+Footer
+© 2022 Gi
