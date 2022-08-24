2b3a13096748fce22cf07d1f8923fb007414e2a6
Student <meetmpatel20-@gnu.ac.in>
2022-08-24 10:35:06 +0530

    initial commit

---
+++ src/main/kotlin/pr1_2.kt
@@ -0,0 +1,14 @@
+fun main(){
+    val n: Int = 10
+    val d: Double = n.toDouble()
+    println("Integer value: $n")
+    println("Double value: $d")
+
+    val s: String = "10"
+    val i: Int = s.toInt()
+    println("String value: $s")
+    println("Double value (To string): $i")
+
+    val d1: Double = s.toDouble()
+    println("Double value (To string): $d1")
+}
